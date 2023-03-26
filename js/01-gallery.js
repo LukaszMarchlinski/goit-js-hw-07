ba79dd9063d8fc6e119d5578b461d48693306c94
Lukasz Marchlinski <lukaszm0409@gmail.com>
2023-03-26 16:57:55 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//tworzenie galerii obrazków
+const allItems = galleryItems.map((galleryItem) => {
+    const item = `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" />
+        </a>
+        </div>`;
+    return item; 
+})
+    .join('');
+
+
+//dodano galerie obrazków
+// gallery.innerHTML = allItems;
+gallery.insertAdjacentHTML("beforeend", allItems);
+
+
+gallery.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    } 
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" />`);
+    instance.show();
+    
+    gallery.addEventListener("keydown", event => {
+        if (event.key === "Escape") {
+            instance.close();
+        }
+    });
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//tworzenie galerii obrazków
+const allItems = galleryItems.map((galleryItem) => {
+    const item = `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`;
+    return item; 
+})
+    .join('');
+
+gallery.innerHTML = allItems;
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
